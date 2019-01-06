62933ff9edd89fb6a159cd71c477d5034afecfcd
Horace JM <nicmay@gmail.com>
2019-01-06 13:59:41 -0600

    updating

---
+++ bamazonCustomer.js
@@ -0,0 +1,68 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+});
+
+connection.connect(function (error) {
+    if (error) throw error;
+    console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+});
+
+function readProducts() {
+    connection.query("SELECT * FROM products", function (error, result) {
+        if (error) throw error;
+        
+        console.log("~~~~~~~~~~ BAMAZON ~~~~~~~~~~");
+        console.log("~~~~~~~~~~ Today's inventory ~~~~~~~~~~");
+        for (var i = 0; i < result.length; i++) {
+            console.log("Item ID: " + result[i].item_id + " || Product Name: " + result[i].product_name + " || Price: $" + result[i].price);
+        }
+        chooseProduct(result);
+    });
+};
+
+function chooseProduct(result) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "ID",
+            message: "Please input the item ID of the item you would like to purchase: "
+        },
+        {
+            type: "input",
+            name: "numToBuy",
+            message: "How many items would you like to purchase?",
+        }
+    ]).then(function (userResponses) {
+        for (var i = 0; i < (result.length); i++) {
+            if (result[i].item_id == userResponses.ID) {
+                chosenItem = result[i];
+                
+                if (((chosenItem.stock_quantity) - (userResponses.numToBuy)) > 0) {
+                    connection.query("UPDATE products SET stock_quantity='" + parseInt(result[i].stock_quantity - userResponses.numToBuy) + "' WHERE item_id ='" + parseInt(result[i].item_id) + "'", function (error, result) {
+                        if (error) throw error;
+                        console.log("Purchase of " + parseInt(userResponses.numToBuy) + " " + chosenItem.product_name + " complete. The total ammount to be charged is $" + parseInt(userResponses.numToBuy * chosenItem.price));
+                        readProducts();
+                    })
+                } else if (userResponses.numToBuy > result[i].stock_quantity) {
+                    console.log("Insufficient quantity!");
+                    readProducts();
+                } else {
+                    console.log("Invalid command");
+                    readProducts();
+                }
+            // } else if ((userResponses.ID > result.length) || ((typeof userResponses.ID) === "string") || ((typeof userResponses.numToBuy) === "string") ){
+            //     console.log("Invalid command");
+            //     readProducts();
+            }        
+
+        }
+    })
+}
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -14,15 +14,15 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Ferrero Rocher", "Chocolate", 5.95, 1000),
-		("Reebok High-Tops", "Footwear", 95, 500),
-		("The Road to Wigan Pier", "Books", 14.95, 100),
-		("Ferrero Rocher", "Chocolate", 5.95, 1000),
-		("Ferrero Rocher", "Chocolate", 5.95, 1000),
-		("Ferrero Rocher", "Chocolate", 5.95, 1000),
-		("Ferrero Rocher", "Chocolate", 5.95, 1000),
-		("Ferrero Rocher", "Chocolate", 5.95, 1000),
-		("Ferrero Rocher", "Chocolate", 5.95, 1000),
-		("Ferrero Rocher", "Chocolate", 5.95, 1000);
+VALUES ("Ferrero Rocher", "Chocolate", 5.95, 10000),
+		("Sandals", "Footwear", 29.95, 10000),
+		("The Road to Wigan Pier", "Books", 14.95, 1000),
+		("In the First Circle", "Books", 19.95, 1000),
+		("Apple AirPods", "Electronics", 185.95, 10000),
+		("Cannon Rebel", "Electronics", 450, 5000),
+		("Nest Thermostat", "Electronics", 230, 20000),
+		("The Larry Sanders Show Box Set", "Movies & TV", 60, 3000),
+		("Squatty Potty", "Lifestyle", 5.95, 20000),
+		("Pampers Easy Ups Box", "Baby Products", 45, 50000);
         
 SELECT * FROM products;
